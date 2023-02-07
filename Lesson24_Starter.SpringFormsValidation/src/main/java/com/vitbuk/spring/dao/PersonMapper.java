ce791bfb037c04919c1779c7c569c3b32fe3c0fe
VitBuk <vitbuk@gmail.com>
2023-02-07 22:36:56 +0200

    JDBC Template learning

---
+++ Lesson24_Starter.SpringFormsValidation/src/main/java/com/vitbuk/spring/dao/PersonMapper.java
@@ -0,0 +1,20 @@
+package com.vitbuk.spring.dao;
+
+import com.vitbuk.spring.models.Person;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper {
+    @Override
+    public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setAge(resultSet.getInt("age"));
+        person.setEmail(resultSet.getString("email"));
+
+        return person;
+    }
+}
